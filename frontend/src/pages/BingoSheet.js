9cf1df92c3240ccba230c6d600bf55c8336e3a29
HERMES <lorenzharfst@gmail.com>
2023-10-30 19:56:26 +0000

    Enable fetching the id off the route with useParam

--- frontend/src/pages/BingoSheet.js
+++ frontend/src/pages/BingoSheet.js
@@ -1,16 +1,18 @@
-import  React, { useState } from 'react';
+import React, { useState } from 'react';
 import BingoDataService from '../services/bingo.service';
+import { useParams } from 'react-router-dom';
 
 import './BingoSheet.css';
 
 const BingoSheet = (props) => {
 	const [bingoMatrix, setBingoMatrix] = useState();
+	const { id } = useParams();
 	
 	// TODO: find a bingo by ID (props.id) and set it to bingoMatrix
 	// TODO: Turn the list of tiles into a 5x5 grid
 	// TODO: Put a listener on each tile to toggle that tile value in the db and set use state
 	return 	<>
-		<div className="bingoSheet"><h1>Test</h1></div>
+		<div className="bingoSheet"><h1>{id}</h1></div>
 		</>;
 }
 
