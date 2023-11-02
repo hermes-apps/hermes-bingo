dbde5738c7cb4aaf3585371bff5da6c7c999763e
HERMES <lorenzharfst@gmail.com>
2023-11-02 22:51:24 +0000

    Add comments to BingoSheet.js

--- frontend/src/pages/BingoSheet.js
+++ frontend/src/pages/BingoSheet.js
@@ -5,14 +5,14 @@ import { useParams } from 'react-router-dom';
 import './BingoSheet.css';
 
 const BingoSheet = (props) => {
-	const [bingoMatrix, setBingoMatrix] = useState([]);
-	const { id } = useParams();
-	const [bingo, setBingo] = useState({title: 'loading'});
+	const [bingoMatrix, setBingoMatrix] = useState([]); // This is a two dimensional array holding the sheet, representing a 5x5 grid, the first array contains the row (0 to 4), the arrays inside contain each tile object (id, text)
+	const { id } = useParams(); // id taken from the route
+	const [bingo, setBingo] = useState({title: 'loading'}); // Raw bingo object from the backend, containing an object with a title and a tiles array inside, each containing attributes sentence and status (which can be 0 and 1). This can obviously be optimised, but I can't be arsed
+	// Function that turns the tiles attribute of the Bingo object and turns it the bingoMatrix 2d array
 	const createBingoMatrix = array => {
-		const iterator = array.values();
-		let returnMatrix = [];
-		let tileRow = [];
-		let actualIndex = 0;
+		let returnMatrix = []; // matrix to be assigned finally to bingoMatrix
+		let tileRow = []; // each element inside returnMatrix contains a row with 5 elements in it
+		let actualIndex = 0; // used to assign an id (and ultimately key) to each Tile
 		for (var i = 0;i < 5;i++){
 			for (var j = 0;j < 5;j++){
 				tileRow.push({id: actualIndex, text: array[actualIndex].sentence});
@@ -24,7 +24,7 @@ const BingoSheet = (props) => {
 
 		setBingoMatrix(returnMatrix);	
 	}
-
+	// Function ran on load to get the Bingo object from backend with this page's assigned id
 	const loadBingo = (id) => {
 		BingoDataService.get(id)
 			.then(response => {
@@ -49,8 +49,8 @@ const BingoSheet = (props) => {
 	useEffect(() => {
 		loadBingo(id);	
 	}, []); // empty dependencies array
-	
-	// TODO: map bingoMatrix into jsx
+
+	// TODO: Create proper React element for Tile
 	// TODO: Put a listener on each tile to toggle that tile value in the db and set use state
 	return 	<div>
 			<h1>{bingo.title}</h1>
