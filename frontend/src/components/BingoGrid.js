6484ec6a324bab1ff451ec73e6919a928cff6c1c
HERMES <lorenzharfst@gmail.com>
2023-11-05 20:41:10 +0000

    Move the retreived Bingo object to BingoGrid as opposed to the page holding said component

--- frontend/src/components/BingoGrid.js
+++ frontend/src/components/BingoGrid.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './BingoGrid.css';
 
 const BingoGrid = props => {
-	// Get the bingoMatrix from props (passed from BingoSheet.js)
-	const bingoMatrix = props.bingoMatrix;
+	const [bingoMatrix, setBingoMatrix] = useState([]); // This is a two dimensional array holding the sheet, representing a 5x5 grid, the first array contains the row (0 to 4), the arrays inside contain each tile object (id, text)
 
 	function handleClick(tileIndex) {
 		// TODO: Add a API call to toggle the state of the clicked Tile
 		alert(`You clicked on tile #${tileIndex}`);
 	}
+
+	// Function that turns the tiles attribute of the Bingo object into the bingoMatrix 2d array
+	const createBingoMatrix = array => {
+		let returnMatrix = []; // matrix to be assigned finally to bingoMatrix
+		let tileRow = []; // each element inside returnMatrix contains a row with 5 elements in it
+		let actualIndex = 0; // used to assign an id (and ultimately key) to each Tile
+		for (var i = 0;i < 5;i++){
+			for (var j = 0;j < 5;j++){
+				tileRow.push({id: actualIndex, text: array[actualIndex].sentence});
+				actualIndex++;
+			}
+			returnMatrix.push(tileRow);
+			tileRow = [];
+		}
+
+		setBingoMatrix(returnMatrix);	
+	}
+
+	// Run this on load
+	useEffect(() => {
+		createBingoMatrix(props.bingo.tiles); // TODO: Move this to BingoGrid.js
+	}, []); // empty dependencies array
 
 	// Return the 5x5 grid
 	return <div className="BingoGrid">
--- frontend/src/pages/BingoSheet.js
+++ frontend/src/pages/BingoSheet.js
@@ -6,31 +6,14 @@ import './BingoSheet.css';
 import BingoGrid from '../components/BingoGrid';
 
 const BingoSheet = (props) => {
-	const [bingoMatrix, setBingoMatrix] = useState([]); // This is a two dimensional array holding the sheet, representing a 5x5 grid, the first array contains the row (0 to 4), the arrays inside contain each tile object (id, text)
 	const { id } = useParams(); // id taken from the route
 	const [bingo, setBingo] = useState({title: 'loading'}); // Raw bingo object from the backend, containing an object with a title and a tiles array inside, each containing attributes sentence and status (which can be 0 and 1). This can obviously be optimised, but I can't be arsed
-	// Function that turns the tiles attribute of the Bingo object and turns it the bingoMatrix 2d array
-	const createBingoMatrix = array => {
-		let returnMatrix = []; // matrix to be assigned finally to bingoMatrix
-		let tileRow = []; // each element inside returnMatrix contains a row with 5 elements in it
-		let actualIndex = 0; // used to assign an id (and ultimately key) to each Tile
-		for (var i = 0;i < 5;i++){
-			for (var j = 0;j < 5;j++){
-				tileRow.push({id: actualIndex, text: array[actualIndex].sentence});
-				actualIndex++;
-			}
-			returnMatrix.push(tileRow);
-			tileRow = [];
-		}
-
-		setBingoMatrix(returnMatrix);	
-	}
+	
 	// Function ran on load to get the Bingo object from backend with this page's assigned id
 	const loadBingo = (id) => {
 		BingoDataService.get(id)
 			.then(response => {
 				setBingo(response.data);
-				createBingoMatrix(response.data.tiles);
 				console.log(response.data);
 			})
 			.catch(err => {
@@ -46,7 +29,7 @@ const BingoSheet = (props) => {
 			});
 	}
 
-	//// Run this on load
+	// Run this on load
 	useEffect(() => {
 		loadBingo(id);	
 	}, []); // empty dependencies array
@@ -54,8 +37,8 @@ const BingoSheet = (props) => {
 	// TODO: Create proper React element for Tile
 	// TODO: Put a listener on each tile to toggle that tile value in the db and set use state
 	return 	<div style={{height: '100%'}}>
-		<div style={{height: '5%', textAlign: 'center', vertiicalAlign: 'middle', margin: '0px', minHeight: '30px'}}><h1 style={{margin: '0px'}}>{bingo.title}</h1></div>
-			<BingoGrid id={id} bingoMatrix={bingoMatrix} />
+		<div style={{height: '5%', textAlign: 'center', vertiicalAlign: 'middle', margin: '0px', minHeight: '30px'}}><h1 style={{margin: '0px'}}>{bingo.title && bingo.title}</h1></div>
+		{bingo.tiles && <BingoGrid id={id} bingo={bingo} />}
 		</div>;	
 
 }
